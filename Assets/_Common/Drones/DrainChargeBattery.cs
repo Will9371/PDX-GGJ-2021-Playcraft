ed4442a3793f5b87f0aae3f667758b3a09e52e0e
WillPetillo <will.petillo@gmail.com>
2021-01-30 14:17:22 -0800

    Auto-braking balance
    No energy cost for braking while not moving.  Drones slow to a stop while docked and not controlled by player.

--- Assets/_Common/Drones/DrainChargeBattery.cs
+++ Assets/_Common/Drones/DrainChargeBattery.cs
@@ -3,10 +3,11 @@
 public class DrainChargeBattery : MonoBehaviour
 {
     [SerializeField] StoredFloatToPercent battery;
-    [SerializeField] ZeroGravityMovement engines;
+    [SerializeField] Rigidbody rb;
     
     public float thrustDrain;
     public float brakeDrain;
+    public float minVelocityForBrakeDrain = 0.25f;
     
     float _thrustDrain => drain ? thrustDrain : 0f;
     float _brakeDrain => drain ? brakeDrain : 0f;
@@ -15,5 +16,10 @@ public class DrainChargeBattery : MonoBehaviour
     public void SetDrain(bool value) { drain = value; }
     
     public void Trust() { battery.AddOverTime(-_thrustDrain); }
-    public void Brake() { battery.AddOverTime(-_brakeDrain); }
+    
+    public void Brake() 
+    {
+        if (rb.velocity.magnitude < minVelocityForBrakeDrain) return; 
+        battery.AddOverTime(-_brakeDrain); 
+    }
 }

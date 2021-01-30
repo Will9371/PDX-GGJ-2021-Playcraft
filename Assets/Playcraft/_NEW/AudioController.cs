bbede8176d391379d335d5e5d8f3a8fb13e240f0
Ted Carter <onewhocarts@gmail.com>
2021-01-29 23:15:57 -0800

    Adding audio hooks for drone movement

---
+++ Assets/Playcraft/_NEW/AudioController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    AudioSource source;
+    float volumeTarget = 1f;
+
+    Coroutine currentRoutine;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void SetVolumeTarget(float target)
+    {
+        volumeTarget = target;
+    }
+
+    public void FadeIn(float duration)
+    {
+        CancelRunningRoutine();
+
+        volumeTarget = 1f;
+        source.volume = 0f;
+        source.Play();
+        currentRoutine = StartCoroutine(FadeRoutine(duration));
+    }
+
+    public void FadeToTarget(float duration)
+    {
+        CancelRunningRoutine();
+
+        currentRoutine = StartCoroutine(FadeRoutine(duration));
+    }
+
+    public void FadeOut(float duration)
+    {
+        CancelRunningRoutine();
+
+        volumeTarget = 0f;
+        currentRoutine = StartCoroutine(FadeRoutine(duration));
+    }
+
+    IEnumerator FadeRoutine(float duration)
+    {
+        var t = 0f;
+        var startingVolume = source.volume;
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            source.volume = startingVolume + t / duration * (volumeTarget - startingVolume);
+            yield return null;
+        }
+        source.volume = volumeTarget;
+    }
+
+    void CancelRunningRoutine()
+    {
+        if (currentRoutine == null)
+            return;
+
+        StopCoroutine(currentRoutine);
+        currentRoutine = null;
+    }
+}

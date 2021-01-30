f386c493e2e5f7beba99f871a55a7e30a4f5d07d
Ted Carter <onewhocarts@gmail.com>
2021-01-30 12:10:27 -0800

    Added audio beacon. Launch from Explore drone with right mouse button

---
+++ Assets/_Common/Drones/FireProjectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireProjectile : MonoBehaviour
+{
+    public GameObject Projectile;
+    public float Speed = 2f;
+
+    public void Fire()
+    {
+        var projectile = Instantiate(Projectile, transform.position, transform.rotation);
+        var rb = projectile.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.velocity = transform.forward * Speed;
+        }
+    }
+}
---
+++ Assets/_Common/Drones/FreezePositionOnCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezePositionOnCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        var rb = GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.isKinematic = true;
+    }
+}

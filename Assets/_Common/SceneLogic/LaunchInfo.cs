7a30a220235a56edcf96df0edb9c95689fad4381
Ted Carter <onewhocarts@gmail.com>
2021-01-30 11:26:59 -0800

    Added scene loader so we can launch from any scene. Tweaked controls

---
+++ Assets/_Common/SceneLogic/LaunchInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LaunchInfo", menuName = "ScriptableObjects/LaunchInfo", order = 1)]
+public class LaunchInfo : ScriptableObject
+{
+    public string StartingScene;
+}

1beaaf703e3339afee4ef8da69bc1ade2b8ac53a
eaclou <eaclou@users.noreply.github.com>
2021-01-30 11:41:35 -0800

    WallPanelScript and Prefab

---
+++ Assets/Env_Prefabs/WallPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallPanel : MonoBehaviour
+{
+    public WallPanelType panelType = WallPanelType.Blank;
+    public enum WallPanelType {
+        Blank,
+        Flat,
+        Cargo,
+        Science,
+        LifeSupport,
+        Multi
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //editor script instead?
+        
+        // Select wall mesh based on panelType
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

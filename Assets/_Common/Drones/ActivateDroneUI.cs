e4abf93a933bc6e2428302addad149c6cf75cebf
WillPetillo <will.petillo@gmail.com>
2021-01-30 23:10:22 -0800

    Applied Swap Panel prefab to System scene
    Hooked up buttons to drone swap logic.  Added warning message to replace button for undocked drones.

---
+++ Assets/_Common/Drones/ActivateDroneUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ActivateDroneUI : MonoBehaviour
+{
+    [SerializeField] GameObject selectButton;
+    [SerializeField] GameObject warningMessage;
+    
+    public void SetActive(bool value)
+    {
+        selectButton.SetActive(value);
+        warningMessage.SetActive(!value);
+    }
+}

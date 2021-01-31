0b7b2cafc851ce46b93326b99e272aa3d7528d8f
WillPetillo <will.petillo@gmail.com>
2021-01-31 14:26:05 -0800

    Added opening narration

---
+++ Assets/_Common/Narrative/OpeningMessage.cs
@@ -0,0 +1,25 @@
+﻿using Playcraft.Dialog;
+using UnityEngine;
+
+public class OpeningMessage : MonoBehaviour
+{
+    [SerializeField] TypewriterText typewriter;
+    [SerializeField] string[] lines;
+
+    void Start() { GenerateMessage(); }
+    
+    void GenerateMessage()
+    {
+        var message = "";
+    
+        for (int i = 0; i < lines.Length; i++)
+        {
+            message += lines[i];
+            
+            if (i < lines.Length - 1)
+                message += "\n";
+        }
+        
+        typewriter.Input(message);
+    }
+}

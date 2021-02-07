1b455d0daf38a15abe93eaa021b5c1e5d26b558b
WillPetillo <will.petillo@gmail.com>
2021-02-06 23:15:56 -0800

    Modified opening screen UI

---
+++ Assets/_Common/Narrative/SyncColor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SyncColor : MonoBehaviour
+{
+    [SerializeField] Color color;
+    [SerializeField] Text[] textElements;
+    [SerializeField] Image[] imageElements;
+    
+    void OnValidate()
+    {
+        foreach (var text in textElements)
+            text.color = color;
+        foreach (var image in imageElements)
+            image.color = color;
+    }
+}

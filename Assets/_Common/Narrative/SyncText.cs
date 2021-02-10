2e16a2b6c008b384a6a00408f2364c9e5e5723fa
WillPetillo <will.petillo@gmail.com>
2021-02-09 16:22:22 -0800

    Ending narration updated

---
+++ Assets/_Common/Narrative/SyncText.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SyncText : MonoBehaviour
+{
+    [SerializeField] int fontSize;
+    [SerializeField] Text[] syncedText;
+
+    void OnValidate()
+    {
+        foreach (var text in syncedText)
+        {
+            text.fontSize = fontSize;
+        }
+    }
+}
